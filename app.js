ba6a19eb959ce0e664d619aa5e1b730a34c5cc1f
Amdrii <andreyk557@gmail.com>
2023-01-14 11:53:56 +0200

    feat[fun-color-cok]

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const cols = document.querySelectorAll(".col");
 
 document.addEventListener('keydown', (event) => {
+    event.preventDefault()
     if (event.code.toLowerCase() === 'space'){
         setPandomColors()
     }
@@ -10,7 +11,12 @@ document.addEventListener('click', event => {
     const type = event.target.dataset.type
 
     if (type == 'lock'){
-        console.log('loooo')
+        const node = event.target.tagName.toLowerCase() === 'i'
+        ? event.target
+        : event.target.children[0]
+
+    node.classList.toggle('fa-lock-open')
+    node.classList.toggle('fa-lock')
     }
 })
 
@@ -28,10 +34,15 @@ function generateRandomColor(){
 
 
 function setPandomColors(){
-    cols.forEach(col => {
+    cols.forEach((col) => {
+        const isLocked = col.querySelector('i').classList.contains('fa-lock')
         const text = col.querySelector('h2')
         const button = col.querySelector('button')
         const color = chroma.random()
+
+        if(isLocked){
+            return
+        }
 
         text.textContent = color
         col.style.background = color
