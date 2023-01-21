2f88bdb7e3373f5fec16dcffd879f1abb0a5d5d6
Amdrii <andreyk557@gmail.com>
2023-01-21 12:39:43 +0200

    feat[test/new/desin]

--- app.js
+++ app.js
@@ -1,109 +1,124 @@
-const cols = document.querySelectorAll(".col");
+const buttonget = document.querySelector(".Togbutton")
+const parent = document.querySelector('#parent')
 
-document.addEventListener('keydown', (event) => {
-    event.preventDefault()
-    if (event.code.toLowerCase() === 'space'){
-        setPandomColors()
-    }
-})
+let isRotate = false
 
-document.addEventListener('click', event => {
-    const type = event.target.dataset.type
-
-    if (type == 'lock'){
-        const node = event.target.tagName.toLowerCase() === 'i'
-        ? event.target
-        : event.target.children[0]
-
-    node.classList.toggle('fa-lock-open')
-    node.classList.toggle('fa-lock')
-    } else if (type === 'copy'){
-        copyToClickboard(event.target.textContent)
-    }
-})
-
-
-
-function generateRandomColor(){
-    const hexCodes = '0123456789ABCDEF'
-
-    let color = ''
-    for(let i = 0; i < 6; i++){
-        color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
-    }
-    return '#' + color
+function toggle() {
+   isRotate = !isRotate
+   parent.setAttribute('data-rotate', isRotate)
 }
-
-function copyToClickboard(text){
-   return navigator.clipboard.writeText(text)
-}
+buttonget.onclick = toggle
 
 
-function setPandomColors(isInitial){
-    const colors = isInitial ? getColorsFromHash() : []
 
-    cols.forEach((col, index) => {
-        const isLocked = col.querySelector('i').classList.contains('fa-lock')
-        const text = col.querySelector('h2')
-        const button = col.querySelector('button')
-        
-
-        if(isLocked){
-            colors.push(text.textContent)
-            return
+{
+    const cols = document.querySelectorAll(".grandChild");
+    
+    document.addEventListener('keydown', (event) => {
+        event.preventDefault()
+        if (event.code.toLowerCase() === 'space'){
+            setPandomColors()
         }
-
-        const color = isInitial 
-        ? colors[index]
-            ? colors[index]
-            : chroma.random() 
-        : chroma.random()
-
-        if(!isInitial){
-            colors.push(color)
-        }
-
-        text.textContent = color
-        col.style.background = color
-
-
-        setTextColor(text, color)
-        setTextColor(button, color)
     })
-
-    updateColorsHash(colors)
-}
-
-function setTextColor(text, color){
-    const luminance = chroma(color).luminance()
-    text.style.color = luminance > 0.5 ? 'black' : 'white'
-}
-
-
-function updateColorsHash(colors = []){
-    document.location.hash = colors.map(col => {
-        return col.toString().substring(1)
-    }).join('-')
-}
-
-function getColorsFromHash(){
-    if (document.location.hash.length > 1){
-      return  document.location.hash
-      .substring(1)
-      .split('-')
-      .map(color => '#' + color)
+    
+    document.addEventListener('click', event => {
+        const type = event.target.dataset.type
+    
+        if (type == 'lock'){
+            const node = event.target.tagName.toLowerCase() === 'i'
+            ? event.target
+            : event.target.children[0]
+    
+        node.classList.toggle('fa-lock-open')
+        node.classList.toggle('fa-lock')
+        } else if (type === 'copy'){
+            copyToClickboard(event.target.textContent)
+        }
+    })
+    
+    
+    
+    function generateRandomColor(){
+        const hexCodes = '0123456789ABCDEF'
+    
+        let color = ''
+        for(let i = 0; i < 6; i++){
+            color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+        }
+        return '#' + color
     }
-    return []
-}
+    
+    function copyToClickboard(text){
+       return navigator.clipboard.writeText(text)
+    }
+    
+    
+    function setPandomColors(isInitial){
+        const colors = isInitial ? getColorsFromHash() : []
+    
+        cols.forEach((col, index) => {
+            const isLocked = col.querySelector('i').classList.contains('fa-lock')
+            const text = col.querySelector('h2')
+            const button = col.querySelector('.butnchan')
+            
+    
+            if(isLocked){
+                colors.push(text.textContent)
+                return
+            }
+    
+            const color = isInitial 
+            ? colors[index]
+                ? colors[index]
+                : chroma.random() 
+            : chroma.random()
+    
+            if(!isInitial){
+                colors.push(color)
+            }
+    
+            text.textContent = color
+            col.style.background = color
+    
+    
+            setTextColor(text, color)
+            setTextColor(button, color)
+        })
+    
+        updateColorsHash(colors)
+    }
+    
+    function setTextColor(text, color){
+        const luminance = chroma(color).luminance()
+        text.style.color = luminance > 0.5 ? 'black' : 'white'
+    }
+    
+    
+    function updateColorsHash(colors = []){
+        document.location.hash = colors.map(col => {
+            return col.toString().substring(1)
+        }).join('-')
+    }
+    
+    function getColorsFromHash(){
+        if (document.location.hash.length > 1){
+          return  document.location.hash
+          .substring(1)
+          .split('-')
+          .map(color => '#' + color)
+        }
+        return []
+    }
+    
+    
+    setPandomColors(true)
+    
+    
+    
+    
+    
+    
+    
 
 
-setPandomColors(true)
-
-
-
-
-
-
-
-
-
+}    
